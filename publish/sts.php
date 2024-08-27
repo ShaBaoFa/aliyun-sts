cd2a1beeaa876191687db38d7a4322448891ef2d
wlfpanda <wlfpanda1012@gmail.com>
2024-08-27 12:00:43 +0800

    简化sts配置

--- publish/sts.php
+++ publish/sts.php
@@ -12,6 +12,9 @@ declare(strict_types=1);
 use function Hyperf\Support\env;
 
 return [
+    /**
+     * STS相关
+     */
     'access_key_id' => env('STS_ACCESS_KEY_ID', 'LTAI4---------44pg8'),
     'access_key_secret' => env('STS_ACCESS_KEY_SECRET', 'oibIsys6xl---------ETocTR'),
     'role_arn' => env('STS_ROLE_ARN', 'acs:ram::xxxxxxxxxxx:role/xxx'),
@@ -19,43 +22,7 @@ return [
     'endpoint' => env('STS_ENDPOINT', 'sts.cn-hangzhou.aliyuncs.com'),
     'duration_seconds' => env('STS_DURATION_SECONDS', 3000),
     'external_id' => env('STS_EXTERNAL_ID', 'external_id_test'),
-    /**
-     * 授予RAM用户使用OSS命令行工具访问目录mybucket/hangzhou/2014/和mybucket/hangzhou/2015/并列举目录中文件的权限.
-     *
-     * RAM用户不清楚目录中有哪些文件，可以使用OSS命令行工具或API直接获取目录信息，此场景下需要添加ListObjects权限。
-     */
-    'policy' => [
-        'Version' => '1',
-        'Statement' => [
-            [
-                'Effect' => 'Allow',
-                'Action' => [
-                    'oss:GetObject',
-                ],
-                'Resource' => [
-                    'acs:oss:*:*:mybucket/hangzhou/2014/*',
-                    'acs:oss:*:*:mybucket/hangzhou/2015/*',
-                ],
-            ],
-            [
-                'Effect' => 'Allow',
-                'Action' => [
-                    'oss:ListObjects',
-                ],
-                'Resource' => [
-                    'acs:oss:*:*:mybucket',
-                ],
-                'Condition' => [
-                    'StringLike' => [
-                        'oss:Prefix' => [
-                            'hangzhou/2014/*',
-                            'hangzhou/2015/*',
-                        ],
-                    ],
-                ],
-            ],
-        ],
-    ],
+
     /**
      * OSS相关.
      */
